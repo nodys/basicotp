021d3d985bb6a9965bd69a81897b0bc712578f36
Jean Ponchon <jean.ponchon@novadiscovery.com>
2016-11-23 14:33:58 +0100

    fix: Support for larger kind of key

--- src/app/api/utils.js
+++ src/app/api/utils.js
@@ -2,11 +2,11 @@ import notp from 'notp'
 import b32 from 'thirty-two'
 
 export function cleanKey (key) {
-  return key.replace(/[^a-z2-7]+/g, '').slice(0, 32)
+  return key.replace(/[^a-z2-7]+/ig, '').toLowerCase()
 }
 
 export function isValidKey (key) {
-  return /^[a-z2-7]{32}$/.test(cleanKey(key))
+  return /^[a-z2-7]+$/.test(cleanKey(key))
 }
 
 export function getCode (key) {
